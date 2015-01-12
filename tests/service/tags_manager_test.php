3df2b33f9585baa3a7a9a3a6cd8f93e2f1bbff81
Robert Heim <robert@robert-heim.de>
2015-01-12 18:08:22 +0100

    fix select query in test

--- tests/service/tags_manager_test.php
+++ tests/service/tags_manager_test.php
@@ -30,7 +30,7 @@ class tags_manager_test extends \phpbb_database_test_case
 		$this->db = $this->new_dbal();
 		$tags_manager = new \robertheim\topictags\service\tags_manager($this->db, null, null, 'phpbb_');
 		$tags_manager->calc_count_tags();
-		$result = $db->sql_query('SELECT count FROM rh_topictags_tags WHERE id=1');
+		$result = $db->sql_query('SELECT count FROM phpbb_rh_topictags_tags WHERE id=1');
 		$count = $db->sql_fetchfield('count');
 		$this->assertEquals($count, 1);
 	}
