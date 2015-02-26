a091ad1f8204f005aa9279e848f83f13850cd502
RobertHeim <Robert.Heim@rwth-aachen.de>
2015-02-26 23:53:37 +0100

    fix upload coverage

--- travis/upload-coverage.sh
+++ travis/upload-coverage.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
-wget https://scrutinizer-ci.com/ocular.phar
-php ocular.phar code-coverage:upload --format=php-clover coverage.clover
+
+set -e
+set -x
+
+DB=$1
+TRAVIS_PHP_VERSION=$2
+
+if [ '$TRAVIS_PHP_VERSION' = '5.3.3' ] && [ '$DB' = 'mysqli' ]; then
+  wget https://scrutinizer-ci.com/ocular.phar
+  php ocular.phar code-coverage:upload --format=php-clover coverage.clover
+fi
