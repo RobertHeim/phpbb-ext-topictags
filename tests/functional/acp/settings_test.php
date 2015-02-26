254e342d7d657adbc8fb3cf586e652060ab948b2
RobertHeim <Robert.Heim@rwth-aachen.de>
2015-02-26 10:17:09 +0100

    refactorings

--- tests/functional/acp/settings_test.php
+++ tests/functional/acp/settings_test.php
@@ -35,45 +35,6 @@ class settings_test extends topictags_functional_test_base
 		$this->assertContainsLang('ACP_TOPICTAGS_MANAGE_TAGS', $crawler->filter('.menu-block')->text());
 	}
 
-	public function test_display_tagloud()
-	{
-		$this->login();
-		$this->admin_login();
-
-		// disable tagcloud
-		$crawler = $this->goto_settings_page();
-		$form = $crawler->selectButton($this->lang('SUBMIT'))->form();
-		$field = $form->get(prefixes::CONFIG . '_display_tagcloud_on_index');
-		$field->setValue(0);
-		$crawler = $this->submit($form);
-		$this->assertContainsLang('TOPICTAGS_SETTINGS_SAVED', $crawler->text());
-
-		// must not be visible on index
-		$crawler = self::request('GET', 'index.php');
-		$this->assertNotContainsLang('RH_TOPICTAGS_TAGCLOUD', $crawler->text());
-
-		// must be disabled
-		$crawler = $this->goto_settings_page();
-		$form = $crawler->selectButton($this->lang('SUBMIT'))->form();
-		$field = $form->get(prefixes::CONFIG . '_display_tagcloud_on_index');
-		$this->assertEquals('0', $field->getValue());
-
-		// enable it
-		$field->setValue(1);
-		$crawler = $this->submit($form);
-		$this->assertContainsLang('TOPICTAGS_SETTINGS_SAVED', $crawler->text());
-
-		// must be enabled now
-		$crawler = $this->goto_settings_page();
-		$form = $crawler->selectButton($this->lang('SUBMIT'))->form();
-		$field = $form->get(prefixes::CONFIG . '_display_tagcloud_on_index');
-		$this->assertEquals('1', $field->getValue());
-
-		// must be visible on the index page
-		$crawler = self::request('GET', 'index.php');
-		$this->assertContainsLang('RH_TOPICTAGS_TAGCLOUD', $crawler->text());
-	}
-
 	/**
 	 * Tests en-/disabling tagging in forum settings as well as en-/disabling tagging
 	 * in all at once via the extensions settings page.
@@ -141,5 +102,44 @@ class settings_test extends topictags_functional_test_base
 		$field = $form->get('rh_topictags_enabled');
 		$this->assertEquals('1', $field->getValue());
 
+	}
+
+	public function test_display_tagloud()
+	{
+		$this->login();
+		$this->admin_login();
+
+		// disable tagcloud
+		$crawler = $this->goto_settings_page();
+		$form = $crawler->selectButton($this->lang('SUBMIT'))->form();
+		$field = $form->get(prefixes::CONFIG . '_display_tagcloud_on_index');
+		$field->setValue(0);
+		$crawler = $this->submit($form);
+		$this->assertContainsLang('TOPICTAGS_SETTINGS_SAVED', $crawler->text());
+
+		// must not be visible on index
+		$crawler = self::request('GET', 'index.php');
+		$this->assertNotContainsLang('RH_TOPICTAGS_TAGCLOUD', $crawler->text());
+
+		// must be disabled
+		$crawler = $this->goto_settings_page();
+		$form = $crawler->selectButton($this->lang('SUBMIT'))->form();
+		$field = $form->get(prefixes::CONFIG . '_display_tagcloud_on_index');
+		$this->assertEquals('0', $field->getValue());
+
+		// enable it
+		$field->setValue(1);
+		$crawler = $this->submit($form);
+		$this->assertContainsLang('TOPICTAGS_SETTINGS_SAVED', $crawler->text());
+
+		// must be enabled now
+		$crawler = $this->goto_settings_page();
+		$form = $crawler->selectButton($this->lang('SUBMIT'))->form();
+		$field = $form->get(prefixes::CONFIG . '_display_tagcloud_on_index');
+		$this->assertEquals('1', $field->getValue());
+
+		// must be visible on the index page
+		$crawler = self::request('GET', 'index.php');
+		$this->assertContainsLang('RH_TOPICTAGS_TAGCLOUD', $crawler->text());
 	}
 }
