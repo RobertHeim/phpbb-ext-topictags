0e1ccbb2a9f3cf16feea34bfd5c6acafc4ffe963
Robert Heim <robert@robert-heim.de>
2015-03-01 11:13:20 +0100

    typo

--- language/en/topictags_acp.php
+++ language/en/topictags_acp.php
@@ -26,7 +26,7 @@ $lang = array_merge($lang, array(
 $lang = array_merge($lang, array(
 	// forum settings page
 	'ACP_RH_TOPICTAGS_ENABLE'								=> 'Enable RH Topic Tags',
-	'ACP_RH_TOPICTAGS_ENABLE_EXP'							=> 'Whether or not to enable tagging of topics in this forum. (When disabling tagging, the tags are NOT REMOVED from the topics in this forum - so when you enable it again, they are still there; If you really want to delete the tags, then use the "Delete tags from this forums" option.)',
+	'ACP_RH_TOPICTAGS_ENABLE_EXP'							=> 'Whether or not to enable tagging of topics in this forum. (When disabling tagging, the tags are NOT REMOVED from the topics in this forum - so when you enable it again, they are still there; If you really want to delete the tags, then use the "Delete tags from this forum" option.)',
 	'ACP_FORUM_SETTINGS_RH_TOPICTAGS_PRUNE'					=> 'Delete tags from this forum',
 	'ACP_FORUM_SETTINGS_RH_TOPICTAGS_PRUNE_EXP'				=> 'This will DELETE all assignments of tags to the topics of this forum. NOTE: To prevent accidental deletion of tags, you need to disabled tagging for this forum.',
 	'ACP_FORUM_SETTINGS_RH_TOPICTAGS_PRUNE_CONFIRM'			=> 'This option will DELETE all assignments of tags to the topics of this forum and you need to disable tagging for this forum, to perform this action.',
