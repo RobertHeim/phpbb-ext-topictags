6c767f9b9ea8062256c431dface6bc366d82d57c
Robert Heim <robert@robert-heim.de>
2015-05-29 21:36:58 +0200

    added functional test

---
+++ tests/functional/main_tests.php
@@ -0,0 +1,93 @@
+<?php
+/**
+ *
+* @package phpBB Extension - RH Topic Tags
+* @copyright (c) 2014 Robet Heim
+* @license http://opensource.org/licenses/gpl-2.0.php GNU General Public License v2
+*/
+namespace robertheim\topictags\tests\functional;
+
+/**
+ * @ignore
+ */
+use \robertheim\topictags\tests\functional\topictags_functional_test_base;
+use \robertheim\topictags\prefixes;
+use robertheim\topictags\tables;
+
+/**
+ * @group functional
+ */
+class main_tests extends topictags_functional_test_base
+{
+
+	/**
+	 * Tests auto prune of tags when topic is deleted
+	 */
+	public function test_prune_tags_when_topic_deleted()
+	{
+		$this->login();
+		$this->admin();
+
+		// enable tagging in forum used for testing
+		$forum_id = 2;
+		$this->enable_topictags_in_forum($forum_id);
+
+		$this->auth->expects($this->exactly(4))
+		->method('acl_getf')
+		->with($this->equalTo('f_read'))
+		->willReturn(array(
+				$forum_id => array(
+						'f_read' => true
+				)
+		));
+
+		// create some topics to work with
+		$tmp = $this->create_topic($forum_id, 'test_prune_tags_when_topic_deleted', 'test topic for test_prune_tags_when_topic_deleted');
+		$topic_id = $tmp['topic_id'];
+		$tmp = $this->create_topic($forum_id, 'test_prune_tags_when_topic_deleted2', 'test topic for test_prune_tags_when_topic_deleted (2)');
+		$topic_id2 = $tmp['topic_id'];
+
+		// add tags
+		$tagname = 'tag1091723409838701993874';
+		$valid_tags = array($tagname);
+		$this->tags_manager->assign_tags_to_topic($topic_id, $valid_tags);
+
+		$tagname2 = 'tag210778577730498403981';
+		$valid_tags = array($tagname2);
+		$this->tags_manager->assign_tags_to_topic($topic_id, $valid_tags);
+		$this->tags_manager->assign_tags_to_topic($topic_id2, $valid_tags);
+
+		// ensure that both tags exist
+		$this->assertEquals(2, sizeof($this->tags_manager->get_existing_tags(array($tagname, $tagname2), true)));
+
+		$this->delete_topic($topic_id2);
+
+		// ensure that the tags are no longer assigned to the deleted topic
+		$result = $this->db->sql_query(
+			'SELECT COUNT(*) as count
+			FROM ' . $table_prefix .
+				 tables::TOPICTAGS . '
+			WHERE topic_id=' . $topic_id2);
+		$assigned_tags_count = $this->db->sql_fetchfield('count');
+		$this->assertEquals(0, $assigned_tags_count);
+
+		// ensure that the tag still is assigned the undeleted topic
+		$result = $this->db->sql_query(
+			'SELECT COUNT(*) as count
+			FROM ' . $table_prefix .
+			tables::TOPICTAGS . '
+			WHERE topic_id=' . $topic_id);
+		$assigned_tags_count = $this->db->sql_fetchfield('count');
+		$this->assertEquals(1, $assigned_tags_count);
+
+		// ensure that the tag that was only assigned to the deleted topic
+		// has been deleted
+		$existing_tags = $this->tags_manager->get_existing_tags(array($tagname2), true);
+		$this->assertEquals(0, sizeof($existing_tags));
+
+		// ensure that the tag that was assigned to both topics
+		// still exists
+		$existing_tags = $this->tags_manager->get_existing_tags(array($tagname), true);
+		$this->assertEquals(1, sizeof($existing_tags));
+	}
+}
