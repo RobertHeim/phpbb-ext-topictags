146e937ac68c98d2250d7c2e3446b1cea4fde9b1
Robert Heim <robert@robert-heim.de>
2015-05-30 09:51:37 +0200

    prepare next release

---
+++ migrations/release_0_0_14.php
@@ -0,0 +1,38 @@
+<?php
+/**
+*
+* @package phpBB Extension - RH Topic Tags
+* @copyright (c) 2014 Robet Heim
+* @license http://opensource.org/licenses/gpl-2.0.php GNU General Public License v2
+*
+*/
+
+namespace robertheim\topictags\migrations;
+
+use robertheim\topictags\prefixes;
+use robertheim\topictags\tables;
+
+class release_0_0_14 extends \phpbb\db\migration\migration
+{
+	protected $version = '0.0.14-RC1';
+
+	public function effectively_installed()
+	{
+		return version_compare($this->config[prefixes::CONFIG.'_version'], $this->version, '>=');
+	}
+
+	public static function depends_on()
+	{
+		return array(
+			'\robertheim\topictags\migrations\release_0_0_13',
+		);
+	}
+
+	public function update_data()
+	{
+		return array(
+			array('config.update', array(prefixes::CONFIG.'_version', $this->version)),
+		);
+	}
+
+}
